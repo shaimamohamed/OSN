f6388090ee7d4459b7928ef5f2bc73540f94d8a9
Shaima <shaima.mohamed.it@gmail.com>
2023-01-21 09:09:00 +0400

    seed

--- OSNAPI/Startup.cs
+++ OSNAPI/Startup.cs
@@ -35,7 +35,7 @@ namespace OSNAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
