48fac0f0f7f6d573abce7c21372adc8343151eaf
shaima Elsheikh <shaima.mohamed.it@gmail.com>
2023-01-23 08:52:34 +0400

    report 2 query

--- Data/Repositories/ReportsRepository.cs
+++ Data/Repositories/ReportsRepository.cs
@@ -55,8 +55,8 @@ namespace Data.Repositories
 
                 //};
                 //result.Add(temp);
-                var parent = _db.students.Where(s => s.ID == item.StudentID).Include(p=>p.Parents).Select(p => p.Parents.FirstOrDefault());
-                item.parents = parent.FirstOrDefault();
+                item.Parents = _db.students.Include(p => p.Parents).FirstOrDefault(s => s.ID == item.StudentID)?.Parents?.ToList();
+                //item.Parents = parent.ToList();
                 
                 result.Add(item);
             }
@@ -66,11 +66,69 @@ namespace Data.Repositories
 
         public List<Report2DTO> Report2()
         {
+            ////var x = _db.Marks.Include("Student").Include("Subject")
+            //var a = _db.Marks.Include(a => a.Student).Include(b => b.Subject).Include(c => c.Student.Parents)
+            //    .Where(f => f.Mark > 90)
+            //    .GroupBy(s => new { s.TermId, s.SubjectId, SubjectName = s.Subject.Name, s.StudentId })
+            //    .Select(r => new Report4DTO
+            //    {
+            //        TermID = r.Key.TermId,
+            //        SubjectID = r.Key.SubjectId,
+            //        SubjectName = r.Key.SubjectName,
+            //        StudentID = r.Key.StudentId
+            //    }).ToList();
 
-            List<Report2DTO> result = new List<Report2DTO>();
-            //var x = _db.students.Include("Parents").Where(f=> f.CurrentTermID==1).ToList();
-            var x = _db.Marks.Include("Student").Include("Subject").Where(f => f.StudentId == f.StudentId && f.Mark > 90).GroupBy(s => s.StudentId).ToList();
-            return result;
+            //var b = a?
+            //    .GroupBy(s => new { s.TermID, s.SubjectID })
+            //    .Where(g => g.Count() >= 2)
+            //    .Select(r => r.ToList()).ToList();
+                
+
+
+            var x = _db.Marks.Include(a => a.Student).Include(b => b.Subject).Include(c => c.Student.Parents)
+                .Where(f => f.Mark > 90)
+                .GroupBy(s => new { s.TermId, s.SubjectId , SubjectName = s.Subject.Name, s.StudentId })
+                //.Where(g => g.Count() >= 2)
+                .Select(r => new Report4DTO
+                {
+                    TermID = r.Key.TermId,
+                    SubjectID = r.Key.SubjectId,
+                    SubjectName = r.Key.SubjectName,
+                    StudentID = r.Key.StudentId
+                }).ToList();
+            //}).Include(s => s.Subjects).Include(s => s.parents).ToList();
+
+            var z = x?.GroupBy(a => new {a.TermID , a.SubjectID})
+                     ?.Where(g => g.Count() >= 2)
+                     ?.Select(a => new Report4DTO { TermID = a.Key.TermID, SubjectID = a.Key.SubjectID , Students = a?.ToList()?.Select(b => _db.students.Include(p => p.Parents)?.FirstOrDefault(s => s.ID == b.StudentID))?.ToList()});
+
+            List<Report4DTO> result = new List<Report4DTO>();
+
+            foreach (var item in x)
+            {
+                //var temp = new Report1DTO
+                //{
+                //StudentID = item.StudentID,
+                //StudentName = item.StudentName,
+                //    Gender = item.Gender,
+                //DOB = item.DOB,
+                //Email = item.Email,
+                //mobile = item.mobile,
+                //CurrentTermName = item.CurrentTermName,
+                //parents = item.parents,
+                //Term = item.Term,
+                //Subjects = item.Subjects,
+                //Marks = item.Marks
+
+                //};
+                //result.Add(temp);
+                item.Parents = _db.students.Include(p => p.Parents).FirstOrDefault(s => s.ID == item.StudentID)?.Parents?.ToList();
+                //item.Parents = parent.ToList();
+
+                result.Add(item);
+            }
+
+            return null;
         }
         public List<Report3DTO> Report3()
         {
